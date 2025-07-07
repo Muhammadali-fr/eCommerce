7018346871eaf9ccc036ae91f3e68c0b0a670e73
Muhammadali-fr <boburjonr934@gmail.com>
2025-07-07 17:06:46 +0500

    array

--- app/components/Famous.tsx
+++ app/components/Famous.tsx
@@ -2,9 +2,14 @@
 // lucide 
 import { ChevronRight } from "lucide-react";
 
+// data 
+import famousItems from "../../data/data";
+
 export default function Famous() {
+    console.log(famousItems);
+
     return (
-        <div className="max-w-[990px] w-[90%] mx-auto space-y-3">
+        <div className="max-w-[990px] w-[90%] mx-auto space-y-3 pb-5">
             <div className="flex items-center">
                 <h1 className="text-2xl font-semibold">Popular</h1>
                 <ChevronRight />
@@ -12,14 +17,18 @@ export default function Famous() {
 
             {/* item here  */}
             <ul className="grid grid-cols-4 gap-5">
-                <li className="w-full h-[400px] rounded-lg overflow-hidden p-2 bg-[#e8e7e5]">
-                    <img className="h-[70%] w-full object-cover object-center rounded-lg" src="/assets/f.png" alt="image" />
-                    <div className="py-2 space-y-1">
-                        <p className="font-semibold">39 000</p>
-                        <p className="text-sm text-gray-800">Erkaklar va ayollar uchun bir xil rangdagi futbolka ,100% paxta</p>
-                        <button className="w-full py-1.5 bg-violet-700 text-white rounded-lg hover:bg-violet-600 cursor-pointer">Add to Cart</button>
-                    </div>
-                </li>
+                {
+                    famousItems.map((item, id) => (
+                        <li className="w-full h-[400px] rounded-lg overflow-hidden p-2 bg-[#e8e7e5] flex items-center justify-between flex-col">
+                            <img className="h-[70%] w-full object-cover object-center rounded-lg" src={item.image} alt={item.description} />
+                            <div className="py-2">
+                                <p className="font-semibold">{item.price}</p>
+                                <p className="text-sm text-gray-800 line-clamp-2 leading-snug">{item.description}</p>
+                            </div>
+                            <button className="w-full py-1.5 bg-violet-700 text-white rounded-lg hover:bg-violet-600 cursor-pointer">Add to Cart</button>
+                        </li>
+                    ))
+                }
             </ul>
 
         </div>
---
+++ data/data.js
@@ -0,0 +1,44 @@
+const famousItems = [
+    {
+        image: "https://images.uzum.uz/d19qdib3uvppu2acfuu0/original.jpg",
+        price: 29090,
+        description: "L'Oreal Parij shampunini o'zgartirish Elseve, orzu uzunligi, keratin bilan, 400 ml"
+    },
+    {
+        image: "https://images.uzum.uz/crglr7vs09cvvbjrhi80/original.jpg",
+        price: 27150,
+        description: "Ayollar uchun dezodorant sprey Nivea Pearl Beauty, marvarid ekstrakti bilan, 150 ml"
+    },
+    {
+        image: "https://images.uzum.uz/d0jgpugn274j5scn1mig/original.jpg",
+        price: 29000,
+        description: "Ayollar va erkaklar uchun bir rangdagi futbolka, bazoviy, paxta, oq"
+    },
+    {
+        image: "https://images.uzum.uz/d0vt66on274lpu38321g/original.jpg",
+        price: 623000,
+        description: "Erkaklar uchun Li-Ning krossovkasi, yugurish uchun, oq"
+    },
+    {
+        image: "https://images.uzum.uz/cq53658sarnfdo9aopg0/original.jpg",
+        price: 269150,
+        description: "Aralashtirgich, vanna uchun dush"
+    },
+    {
+        image: "https://images.uzum.uz/cnas83925kub33f4kf90/original.jpg",
+        price: 291000,
+        description: "O'yin mexanik klaviaturasi T60"
+    },
+    {
+        image: "https://images.uzum.uz/d1ba8mj3uvppu2acrrhg/original.jpg",
+        price: 48000,
+        description: "Futbolkalar erkaklar uchun, erkaklar futbolkasi, 100% paxta"
+    },
+    {
+        image: "https://images.uzum.uz/cn855nrifoubkc6s7pg0/original.jpg",
+        price: 12500,
+        description: "Erkaklar va ayollar uchun uniseks, sport paypoqlar paxtadan, o'lchami 36 dan 44 gacha"
+    }
+]
+
+export default famousItems
