1c90d6537eae45f348225bfab3e9b440447c93db
Muhammadali-fr <boburjonr934@gmail.com>
2025-07-10 22:59:43 +0500

    thumbnail

--- app/(pages)/product/[id]/page.tsx
+++ app/(pages)/product/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { use, useEffect, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 
 // data 
 import famousItems from "../../../../data/data";
@@ -8,60 +8,93 @@ import famousItems from "../../../../data/data";
 // swiper 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-import "swiper/css";  
+import "swiper/css";
 import "swiper/css/navigation";
 
 // lucide 
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-
 export default function ProductPage({ params }: { params: Promise<{ id: number }> }) {
-
   const { id } = use(params);
   const [currentProduct, setCurrentProduct] = useState<any>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevRef = useRef<HTMLButtonElement | null>(null);
+  const nextRef = useRef<HTMLButtonElement | null>(null);
+  const swiperRef = useRef<any>(null);
+
+  console.log(swiperRef.current);
+
 
   useEffect(() => {
-    const product = famousItems.find((item) => {
-      return item.id === Number(id);
-    })
-
+    const product = famousItems.find((item) => item.id === Number(id));
     setCurrentProduct(product);
-  }, [])
+  }, []);
 
   if (!currentProduct) {
-    return (
-      <h1>loading</h1>
-    )
+    return <h1>Loading...</h1>;
   }
 
   return (
-    <div className="w-[90%] max-w-[990px] mx-auto flex items-start justify-between py-5">
-      <div className="w-[50%]">
+    <div className="w-[90%] max-w-[990px] mx-auto flex items-start justify-between py-5 gap-5">
+      <div className="w-[50%] space-y-2">
         <div className="w-full relative">
+          <button
+            ref={prevRef}
+            className="bg-white w-[40px] h-[40px] flex items-center justify-center rounded-full text-gray-800 cursor-pointer hover:bg-gray-300 absolute left-2 top-1/2 -translate-y-1/2 z-10"
+          >
+            <ChevronLeft />
+          </button>
+          <button
+            ref={nextRef}
+            className="bg-white w-[40px] h-[40px] flex items-center justify-center rounded-full text-gray-800 cursor-pointer hover:bg-gray-300 absolute right-2 top-1/2 -translate-y-1/2 z-10"
+          >
+            <ChevronRight />
+          </button>
+
+          {/* Swiper here  */}
           <Swiper
-          navigation={{
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-          }}
+            modules={[Navigation]}
             loop={true}
-            className="rounded-lg relative">
+            navigation={{
+              prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            }}
+            onBeforeInit={(swiper) => {
+              swiper.params.navigation.prevEl = prevRef.current;
+              swiper.params.navigation.nextEl = nextRef.current;
+              swiperRef.current = swiper;
+            }}
+            onSlideChange={(swiper) => {
+              setCurrentIndex(swiper.realIndex);
+            }}
+            className="rounded-lg relative border-3 border-gray-300"
+          >
             {currentProduct.images.map((image: string, index: number) => (
               <SwiperSlide key={index}>
-                <img className="rounded-lg" src={image} alt={`Current image: ${index + 1}`} />
+                <img className="rounded-lg" src={image} alt={`Image ${index + 1}`} />
               </SwiperSlide>
             ))}
-
-            <button className="swiper-button-prev bg-white  w-[40px] h-[40px] flex items-center justify-center rounded-full text-gray-800 cursor-pointer hover:bg-gray-300 absolute left-2 top-1/2 -translate-y-1/2 z-10"><ChevronLeft /></button>
-            <button className="swiper-button-next bg-white  w-[40px] h-[40px] flex items-center justify-center rounded-full text-gray-800 cursor-pointer hover:bg-gray-300 absolute right-2 top-1/2 -translate-y-1/2 z-10"><ChevronRight/></button>
           </Swiper>
+        </div>
+
+        <div className="flex items-center gap-2">
+          {currentProduct.images.map((image: string, index: number) => (
+            <img key={index} onClick={() => swiperRef.current?.slideToLoop(index)} className={`h-[100px] rounded-lg border-3 border-gray-300 cursor-pointer ${currentIndex === index && " border-violet-700"}`} src={image} alt={`Image number: ${index + 1}`} />
+          ))}
         </div>
       </div>
 
+      {/* right  */}
       <div className="w-[50%]">
-        <p>Mahsulot narxi: <span>{currentProduct.price}</span></p>
-        <p>Mahsulot haqida: <span>{currentProduct.description}</span></p>
-        <button>Add to Card</button>
+        <p>
+          Mahsulot narxi: <span>{currentProduct.price}</span>
+        </p>
+        <p>
+          Mahsulot haqida: <span>{currentProduct.description}</span>
+        </p>
+        <button className="w-full py-4 bg-violet-700 text-white rounded-lg cursor-pointer hover:bg-violet-600">Add to Cart</button>
       </div>
     </div>
-  )
+  );
 }
