9620e26f25b7cd9a77c14eb3b07bfb4ec61d834d
Muhammadali-fr <boburjonr934@gmail.com>
2025-08-02 09:52:25 +0500

    update backend server

--- app/api/fetcher.ts
+++ app/api/fetcher.ts
@@ -3,17 +3,17 @@
 import axios, { AxiosRequestConfig } from "axios"
 
 export async function fetcher<T>(
-    url:string,
+    url: string,
     options: AxiosRequestConfig = {}
-): Promise<T> {   
-        const res  = await axios<T>(`http://localhost:8000${url}`, {
-            ...options,
-            headers: {
-                "Content-Type": "application/json",
-                ...options.headers,
-            }
-        });
+): Promise<T> {
+    const res = await axios<T>(`https://round-server.onrender.com${url}`, {
+        ...options,
+        headers: {
+            "Content-Type": "application/json",
+            ...options.headers,
+        }
+    });
 
-        // returnning data 
-        return res.data;
+    // returnning data 
+    return res.data;
 }
